4c0b385909ef99aa7e271fed3e46b1c1cb80232b
vinay-colaco <vinaygreavsoncolaco09@gmail.com>
2023-05-30 21:42:38 +0530

    created intent to go to upload notice

--- SJECAdmin/app/src/main/java/com/example/sjecadmin/MainActivity.java
+++ SJECAdmin/app/src/main/java/com/example/sjecadmin/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.sjecadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout UploadNotice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UploadNotice = (LinearLayout) findViewById(R.id.uploadNotice);
+        UploadNotice.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        Intent it=new Intent(this, com.example.sjecadmin.UploadNotice.class);
+        startActivity(it);
     }
 }
